6b4374079e919de2a1720fe55b431f7f5f40a538
Davis Bickford <davis.bickford@gmail.com>
2019-12-17 22:33:23 -0700

    Updates

---
+++ precache-manifest.24c0fd428c083a343bd781303071663f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74bfac9f81961bba63c12c27e31efebf",
+    "url": "/pug-or-not-pug/index.html"
+  },
+  {
+    "revision": "4608c656372ed7c8488b",
+    "url": "/pug-or-not-pug/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "7d1ad9e7d39bb5feb13d",
+    "url": "/pug-or-not-pug/static/css/main.b0f19cec.chunk.css"
+  },
+  {
+    "revision": "4608c656372ed7c8488b",
+    "url": "/pug-or-not-pug/static/js/2.62f0fd6e.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/pug-or-not-pug/static/js/2.62f0fd6e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7d1ad9e7d39bb5feb13d",
+    "url": "/pug-or-not-pug/static/js/main.12b5e282.chunk.js"
+  },
+  {
+    "revision": "133683f821286893f40b",
+    "url": "/pug-or-not-pug/static/js/runtime-main.ccc4a8fb.js"
+  },
+  {
+    "revision": "55b333b4b180e039b6be053f3cb06d60",
+    "url": "/pug-or-not-pug/static/media/pug_logo.55b333b4.png"
+  }
+]);
