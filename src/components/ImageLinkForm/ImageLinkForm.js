68fef5367bfc1c29f3908d07df93aa7c5d55215d
Davis Bickford <davis.bickford@gmail.com>
2019-12-18 21:51:57 -0700

    styling fixes

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -3,14 +3,14 @@ import './ImageLinkForm.css'
 
 const ImageLinkForm = ({ onInputChange, onKeyPress, onButtonSubmit }) => {
 	return (
-		<div className='ma4 mt0'>
+		<div className=' mt0 w-100'>
 			<p className='f4'>
 				{'This essential app detects pugs in images.'}
 				<br />
 				{'Enter a URL to try it out!'}
 			</p>
-			<div className='center'>
-				<div className='center form stairs pa3 br3 shadow-5'>
+			<div className='w-100'>
+				<div className='w-100 center stairs pa3 br3 shadow-5'>
 					<input className='f4 pa2 w-70 center' type='text' onChange={onInputChange} onKeyPress={onKeyPress} />
 					<button 
 						className='w-30 grow f4 link ph3 pv2 dib white bg-gray'
--- src/components/PugRecognition/PugRecognition.js
+++ src/components/PugRecognition/PugRecognition.js
@@ -6,7 +6,7 @@ const PugRecognition = ( { imageUrl, boxes, isPug, isThinking }) => {
 	if (boxes.length > 0) {
 		boxList = boxes.map((box, i) => {
 			return (
-				<div key={i} className='bounding-box' style={{top: box.topRow, bottom: box.bottomRow, left: box.leftCol, right: box.rightCol}} ></div>
+				<div key={i} className='bounding-box z-1' style={{top: box.topRow, bottom: box.bottomRow, left: box.leftCol, right: box.rightCol}} ></div>
 			)
 		})
 	}
@@ -20,13 +20,13 @@ const PugRecognition = ( { imageUrl, boxes, isPug, isThinking }) => {
 	if (imageUrl.length > 0)
 		return (
 			<div>
-				<div id='result' className='center mv-0 br3 pa2 w-90 w-75-m w-75-s'>
+				<div id='result' className='center mv2 br3 pa2 w-100 shadow-3'>
 					{ pugResult }
 					{ faceResult }
 				</div>
 				<div className='center ma0'>
 					<div className='center absolute mt2 mb5'>
-						<img  alt='' id='inputimage' src={imageUrl} />
+						<img  alt='' id='inputimage' className='br3' src={imageUrl} />
 						{ boxes.length > 0 ? boxList : <div></div> }
 					</div>
 				</div>
